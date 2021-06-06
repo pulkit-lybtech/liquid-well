b83151f2aaf4df9ba2774f4bf81b46cd5bcfc835
pulkit-lyb <pulkit.lybtechnology@gmail.com>
2021-06-06 20:34:03 +0530

    Update Results.js

--- src/views/custom/Results.js
+++ src/views/custom/Results.js
@@ -1,37 +1,53 @@
 import { 
 	CDataTable,
 	CButton,
-	CButtonClose  } from '@coreui/react'
-import {
-	COffcanvas,
-	COffcanvasBody,
-	COffcanvasHeader,
-	COffcanvasTitle,} from '@coreui/utils'
+	CProgress,
+	CBadge,
+	  } from '@coreui/react'
+import CIcon from "@coreui/icons"
 import React from 'react'
 import { Link } from 'react-router-dom'
 
 export default function AllQuest() {
 
-	const [visible, setVisible] = React.useState(false)
 
 	const resultData = [
-		{id: 0, uploaded_dates: '22-05-2021', no_attempts: '5',accurate_attempts: '2', accuracy: '50%', level: 'beginner',plan: 'Plan A'},
+		{id: 0, customer_name: 'Rajat Kumar ', date_created: '27/12/2021',usage: '2',plan: "Plan A",videolink: "", accuracy: '50', level: 'beginner', result: 'Passed',emailed: 'yes', notified: 'true'},
+		{id: 0, customer_name: 'Rajat Kumar ', date_created: '27/12/2021',usage: '2',plan: "Plan A",videolink: "", accuracy: '20', level: 'advance', result: 'Passed',emailed: 'yes', notified: 'false'},
+		{id: 0, customer_name: 'Rajat Kumar ', date_created: '27/12/2021',usage: '2',plan: "Plan A",videolink: "", accuracy: '40', level: 'beginner', result: 'Passed',emailed: 'no', notified: 'true'},
+		{id: 0, customer_name: 'Rajat Gupta A ', date_created: '27/12/2021',usage: '2',plan: "Plan A",videolink: "", accuracy: '50', level: 'beginner', result: 'Passed',emailed: 'yes', notified: 'false'},
+		{id: 0, customer_name: 'Rajat Kumar ', date_created: '27/12/2021',usage: '2',plan: "Plan A",videolink: "", accuracy: '30', level: 'beginner', result: 'Passed',emailed: 'yes', notified: 'true'},
+		{id: 0, customer_name: 'Rajat Kumar ', date_created: '27/12/2021',usage: '3',plan: "Plan A",videolink: "", accuracy: '20', level: 'medium', result: 'Passed',emailed: 'no', notified: 'true'},
+		{id: 0, customer_name: 'Anumata Kumar ', date_created: '27/12/2021',usage: '2',plan: "Plan A",videolink: "", accuracy: '50', level: 'beginner', result: 'Failed',emailed: 'yes', notified: 'true'},
+		{id: 0, customer_name: 'Rajat Kumar ', date_created: '27/12/2021',usage: '2',plan: "Plan A",videolink: "", accuracy: '40', level: 'pro', result: 'Passed',emailed: 'yes', notified: 'false'},
+		{id: 0, customer_name: 'Jai Shankar Kumar ', date_created: '27/12/2021',usage: '2',plan: "Plan A",videolink: "", accuracy: '90', level: 'beginner', result: 'Passed',emailed: 'yes', notified: 'true'},
+		{id: 0, customer_name: 'Rajat Kumar ', date_created: '27/12/2021',usage: '2',plan: "Plan A",videolink: "", accuracy: '50', level: 'beginner', result: 'Passed',emailed: 'yes', notified: 'true'},
+		{id: 0, customer_name: 'Rajat Kumar ', date_created: '27/12/2021',usage: '2',plan: "Plan A",videolink: "", accuracy: '10', level: 'beginner', result: 'Passed',emailed: 'yes', notified: 'false'},
+		{id: 0, customer_name: 'Rajat Kumar ', date_created: '27/12/2021',usage: '2',plan: "Plan A",videolink: "", accuracy: '60', level: 'pro', result: 'Passed',emailed: 'no', notified: 'true'},
 		
 	  ]
 
 
+	  const getBadge = (result)=>{
+		switch (result) {
+		  case 'Passed': return 'success'
+		  case 'Failed': return 'danger'
+		  default: return 'warning'
+		}
+	  }
+
 	const fields = [
 		{key: 'id'},
-		{ key: 'customer_name',_style:{width: "185px"} },
+		{ key: 'customer_name', },
 		'date_created',
 		{key:'usage', },
 		'plan',
-		{key: 'videolink', sorter: false},
-		{ key: 'accuracy'  },
 		'level',
-		{key: 'result',label: '',sorter: false},
-		{key: 'notified',label: '',sorter: false,filter: false },
-		{key: 'emailed',label: '',},
+		{key: 'result'},
+		{key: 'notified',filter: false },
+		{key: 'emailed',},
+		{ key: 'accuracy'  },
+		{key: 'videolink',filter: false , _style: {width: "50px"}, sorter: false},
 	  ]
 
 	  
@@ -44,48 +60,60 @@ export default function AllQuest() {
 			<CDataTable
 			items={resultData}
 			fields={fields}
-			
 			tableFilter
-			
 			itemsPerPageSelect
 			itemsPerPage={5}
 			hover
 			sorter
 			pagination
 			scopedSlots = {{
+				'result':
+				  (item)=>(
+					<td>
+					  <CBadge color={getBadge(item.result)}>
+						{item.result}
+					  </CBadge>
+					</td>
+				  ),
+				'accuracy':
+				  (item)=>(
+					<td className="bg-light text-center ">
+						<small className="text-dark ">{item.accuracy}%</small>
+						<CProgress size="xs" color="success" value={item.accuracy} className="mb-1 bg-white" />
+					</td>
+				  ),
+				'notified':
+				  (item)=>(
+					<td className={ item.notified ? ' text-success' : 'bg-warning  text-dark'}>
+						{item.notified}
+					</td>
+				  ),
 				'videolink':
-				(item)=>(
-					<td className={``}>
-						<CButton onClick={() => setVisible(true)}>
-							Show Video 
+				  (item)=>(
+					<td className="bg-light">
+						<CButton variant="outline" color="dark">
+							Show 
 						</CButton>
-						<COffcanvas
-							placement="end"
-							visible={visible}
-							onDismiss={() => setVisible(false)}
-						>
-							<COffcanvasHeader>
-							<COffcanvasTitle>Offcanvas</COffcanvasTitle>
-							<CButtonClose
-								className="text-reset"
-								onClick={() => setVisible(false)}
-							/>
-							</COffcanvasHeader>
-							<COffcanvasBody>
-							<iframe width="956" height="538" src="https://www.youtube.com/embed/6stlCkUDG_s?list=PL4Gr5tOAPttLOY9IrWVjJlv4CtkYI5cI_" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
-							</COffcanvasBody>
-						</COffcanvas>
-
 					</td>
-				),
-				'emailed':
-				(item)=>(
-					<td className={`banner ${(item==='yes') ? "bg-success" : "bg-danger"}`}>
-						{item}		
-					</td>
-				),
-			}
-		}
+				  ),
+				// 'show_details':
+				//   (item, index)=>{
+				// 	return (
+				// 	  <td className="py-2">
+				// 		<CButton
+				// 		  color="primary"
+				// 		  variant="outline"
+				// 		  shape="square"
+				// 		  size="sm"
+				// 		  onClick={()=>{toggleDetails(index)}}
+				// 		>
+				// 		  {details.includes(index) ? 'Hide' : 'Show'}
+				// 		</CButton>
+				// 	  </td>
+				// 	  )
+				//   },
+				
+			  }}
 			/>
 		
 		</div>
